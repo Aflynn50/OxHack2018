e977574a07ec2c2a62a2bee3c312873a60c521c1
Aflynn50 <alastairflynn@hotmail.co.uk>
2018-11-24 18:04:21 +0000

    added some twitter stuf

--- VictumInfo.py
+++ VictumInfo.py
@@ -1,3 +1,5 @@
+from TwitterSearch import *
+
 class GeoPosition:
     def __init__(self):
         self.longitude = 0
@@ -5,6 +7,17 @@ class GeoPosition:
         self.radius = 0
 
 
-class VictumInfo:
+class VictimInfo:
     def __init__(self):
-        self.location = GeoPosition()
+        self.location = "51.7555,0,100mi"
+        self.tso = TwitterSearchOrder()
+        self.tso.set_geocode(51.7555,0.0,100)
+        self.searchResults = TwitterSearch(consumer_key="eoe5TmgDTsmI0E2NpSrt7KPfg",
+                                           consumer_secret="onNO7vobeUw1piKWfmXQJwmpMKrQbzRWTeXFjKo6nZwipCPJ3r",
+                                           access_token="2491643690-UVYiYfoc3VXyQ4dc5B2VcSCMTU0hsl0www8dB7n",
+                                           access_token_secret="g8RmQzuyIA0vgxfCgTcMsSshbWYGmhmKfIO3320TQ6isY")
+        for tweet in self.searchResults:
+            print('@%s tweeted: %s' % (tweet['user']['screen_name'],tweet['text']))
+
+v = VictimInfo()
+
