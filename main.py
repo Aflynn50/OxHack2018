4a62276b1e98414817252697745e4c022de08feb
AmesingFlank <ldfrank533@outlook.com>
2018-11-24 20:40:44 +0000

    added offline database

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from GeoInfo import GeoPosition,VictimInfo,VictimRegion
 
 
 victims = []
-for i in range(10):
-    victimPos0 = GeoPosition(51.5074, 12.1278, 1)
+for i in range(10000):
+    victimPos0 = GeoPosition(45, 28, 1)
     victim0 = VictimInfo(victimPos0)
     victims.append(victim0)
 
 
-for i in range(10):
+for i in range(10000):
     victimPos0 = GeoPosition(51.5074, 0.1278, 1)
     victim0 = VictimInfo(victimPos0)
     victims.append(victim0)
