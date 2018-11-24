178704c5313cb489f58bcff264b09698aaea92d2
AmesingFlank <ldfrank533@outlook.com>
2018-11-24 16:12:21 +0000

    added victum info

---
+++ VictumInfo.py
@@ -0,0 +1,11 @@
+class GeoPosition:
+    def __init__(self):
+        self.longitude = 0
+        self.latitude = 0
+        self.radius = 0;
+
+
+class VictumInfo:
+    def __init__(self):
+        self.location = GeoPosition()
+        
